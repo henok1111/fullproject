47dc89d7499b2a9943f9a8ad8893fe987089c76d
AmaniTefe <amani123tefe@gmail.com>
2024-04-20 21:59:57 +0300

    added message capability to the add appointment functions

--- frontend/src/adminpage/scenes/Appointment/appointmentform.jsx
+++ frontend/src/adminpage/scenes/Appointment/appointmentform.jsx
@@ -1,12 +1,19 @@
 import { useState, useEffect } from "react";
-import { Autocomplete, TextField, Typography, Box, Button, colors, useTheme } from "@mui/material";
+import {
+  Autocomplete,
+  TextField,
+  Typography,
+  Box,
+  Button,
+  colors,
+  useTheme,
+} from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
 import axios from "axios"; // Import Axios for making HTTP requests
 import { tokens } from "../../../theme";
-  
 
 const Appointmentform = () => {
   const theme = useTheme();
@@ -25,7 +32,7 @@ const Appointmentform = () => {
     try {
       const response = await fetch("http://localhost:8081/api/cases");
       const data = await response.json();
-      console.log("data",data)
+      console.log("data", data);
       setCases(data);
     } catch (error) {
       console.error("Error fetching cases:", error);
@@ -36,9 +43,13 @@ const Appointmentform = () => {
   const fetchPetitionersAndRespondents = async (caseId) => {
     try {
       console.log("Selected Case ID:", caseId); // Log the selected case ID before making requests
-      const petitionersResponse = await axios.get(`http://localhost:8081/api/petitioners/${caseId}`);
-      const respondentsResponse = await axios.get(`http://localhost:8081/api/respondents/${caseId}`);
-  
+      const petitionersResponse = await axios.get(
+        `http://localhost:8081/api/petitioners/${caseId}`
+      );
+      const respondentsResponse = await axios.get(
+        `http://localhost:8081/api/respondents/${caseId}`
+      );
+
       setPetitioners(petitionersResponse.data);
       setRespondents(respondentsResponse.data);
     } catch (error) {
@@ -67,26 +78,33 @@ const Appointmentform = () => {
   const handleSaveAppointment = async () => {
     try {
       // Extract phone numbers from the fetched petitioner and respondent data
-      const petitionerPhoneNumbers = petitioners.map(petitioner => petitioner.mobile_number).join(", ");
-      const respondentPhoneNumbers = respondents.map(respondent => respondent.mobile_number).join(", ");
-  
+      const petitionerPhoneNumbers = petitioners
+        .map((petitioner) => petitioner.mobile_number)
+        .join(", ");
+      const respondentPhoneNumbers = respondents
+        .map((respondent) => respondent.mobile_number)
+        .join(", ");
+
       const appointmentData = {
         case_id: selectedCase.case_id,
         petitioner_phone_numbers: petitionerPhoneNumbers, // Include petitioner's phone numbers
         respondent_phone_numbers: respondentPhoneNumbers, // Include respondent's phone numbers
         date: date,
         time: time,
-        note: note
+        note: note,
       };
-      console.log("Data to be sent to the server:", appointmentData)
-      const response = await axios.post("http://localhost:8081/api/addappointments", appointmentData);
+      console.log("Data to be sent to the server:", appointmentData);
+      const response = await axios.post(
+        "http://localhost:8081/api/addappointments",
+        appointmentData
+      );
       console.log("Appointment saved successfully:", response.data);
       // Optionally, you can navigate to another page or show a success message here
     } catch (error) {
       console.error("Error saving appointment:", error);
     }
   };
-  
+
   return (
     <Box padding="20px" mb="10px" backgroundColor={colors.blueAccent[900]}>
       <Box display="flex" justifyContent="end" mt="10px">
@@ -101,7 +119,12 @@ const Appointmentform = () => {
         </Button>
       </Box>
       <Header title="Add Appointment" subtitle="Appointment Form" />
-      <Box margin="20px" padding="30px" borderRadius="15px" bgcolor="primary.400">
+      <Box
+        margin="20px"
+        padding="30px"
+        borderRadius="15px"
+        bgcolor="primary.400"
+      >
         <Box>
           <Autocomplete
             value={selectedCase}
@@ -119,56 +142,94 @@ const Appointmentform = () => {
         </Box>
         {selectedCase && (
           <Box>
-            <Typography variant="h6" sx={{ mt: "20px" }}>Petitioners:</Typography>
+            <Typography variant="h6" sx={{ mt: "20px" }}>
+              Petitioners:
+            </Typography>
             {petitioners.map((petitioner, index) => (
               <Typography key={index}>
-                Name: {petitioner.first_name} - PhoneNumber: {petitioner.mobile_number} - Email: {petitioner.email}
+                Name: {petitioner.first_name} - PhoneNumber:{" "}
+                {petitioner.mobile_number} - Email: {petitioner.email}
               </Typography>
             ))}
-            <Typography variant="h6" sx={{ mt: "20px" }}>Respondents:</Typography>
-            {respondents.map((respondent, index) =>
- (
-  <Typography key={index}>
-    Name: {respondent.first_name} - PhoneNumber: {respondent.mobile_number} - Email: {respondent.email}
-  </Typography>
-))}
-</Box>
-)}
-<Box display="flex" gap="20px" mt="10px">
-<Typography variant="body1" sx={{ mt: "20px", ml: "20px" }}>Date:</Typography>
-<TextField type="date" variant="outlined" fullWidth value={date} onChange={(e) => setDate(e.target.value)} />
-<Typography variant="body1" sx={{ mt: "20px", ml: "20px" }}>Time:</Typography>
-<TextField type="time" variant="outlined" fullWidth value={time} onChange={(e) => setTime(e.target.value)} />
-</Box>
-<Box mt="10px">
-<TextField
-multiline
-rows={4}
-fullWidth
-id="Note"
-name="Note"
-label="Note"
-variant="outlined"
-sx={{ mt: "20px" }}
-value={note}
-onChange={(e) => setNote(e.target.value)}
-/>
-</Box>
-<Box
-sx={{
-mt: "30px",
-display: "flex",
-justifyContent: "flex-end",
-mb: "50px",
-}}
-gap={2}
->
-<Button type="button" variant="contained" color="error" sx={{ mt: "10px" }} onClick={handleBackButtonClick}>Cancel</Button>
-<Button type="button" variant="contained" color="success" startIcon={<SaveIcon />} sx={{ mt: "10px" }} onClick={handleSaveAppointment}>Save</Button>
-</Box>
-</Box>
-</Box>
-);
+            <Typography variant="h6" sx={{ mt: "20px" }}>
+              Respondents:
+            </Typography>
+            {respondents.map((respondent, index) => (
+              <Typography key={index}>
+                Name: {respondent.first_name} - PhoneNumber:{" "}
+                {respondent.mobile_number} - Email: {respondent.email}
+              </Typography>
+            ))}
+          </Box>
+        )}
+        <Box display="flex" gap="20px" mt="10px">
+          <Typography variant="body1" sx={{ mt: "20px", ml: "20px" }}>
+            Date:
+          </Typography>
+          <TextField
+            type="date"
+            variant="outlined"
+            fullWidth
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <Typography variant="body1" sx={{ mt: "20px", ml: "20px" }}>
+            Time:
+          </Typography>
+          <TextField
+            type="time"
+            variant="outlined"
+            fullWidth
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </Box>
+        <Box mt="10px">
+          <TextField
+            multiline
+            rows={4}
+            fullWidth
+            id="Note"
+            name="Note"
+            label="Note"
+            variant="outlined"
+            sx={{ mt: "20px" }}
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          />
+        </Box>
+        <Box
+          sx={{
+            mt: "30px",
+            display: "flex",
+            justifyContent: "flex-end",
+            mb: "50px",
+          }}
+          gap={2}
+        >
+          <Button
+            type="button"
+            variant="contained"
+            color="error"
+            sx={{ mt: "10px" }}
+            onClick={handleBackButtonClick}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="button"
+            variant="contained"
+            color="success"
+            startIcon={<SaveIcon />}
+            sx={{ mt: "10px" }}
+            onClick={handleSaveAppointment}
+          >
+            Save
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Appointmentform;
